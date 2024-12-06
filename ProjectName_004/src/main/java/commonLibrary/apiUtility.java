c323b18d610ba526aebd02c9f2ef77053a42275a
jitendraprakash786 <jitendraprakash786@outlook.com>
2024-12-06 18:56:48 +0530

    Updated the API steps

--- ProjectName_004/src/main/java/commonLibrary/apiUtility.java
+++ ProjectName_004/src/main/java/commonLibrary/apiUtility.java
@@ -6,13 +6,27 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONObject;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class apiUtility {
     JSONObject requestParams = null;
     RequestSpecification request = null;
 
+    public String getDetailsFromApiTestData(String apiUrl) throws IOException {
+        String retValue = null;
+        String filePath = System.getProperty("user.dir") + "/src/main/java/store/API/staticData.properties";
+        FileInputStream fis = new FileInputStream(filePath);
+        Properties propApi = new Properties();
+        propApi.load(fis);
+        return propApi.getProperty(apiUrl);
+    }
+
     Response response = null;
-    public void setBaseUri(){
-        RestAssured.baseURI = "https://reqres.in/";
+    public void setApiEnvironment(String apiBaseUri) throws IOException {
+        RestAssured.baseURI = getDetailsFromApiTestData(apiBaseUri);
         request = RestAssured.given();
     }
 
--- ProjectName_004/src/test/java/stepDefinitions/apiCommonSteps.java
+++ ProjectName_004/src/test/java/stepDefinitions/apiCommonSteps.java
@@ -1,19 +1,49 @@
 package stepDefinitions;
 
 import commonLibrary.apiUtility;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class apiCommonSteps {
     apiUtility api = new apiUtility();
 
     @When("^I perform POST method on (.*) API$")
-    public void apiProtocolsActions(String apiEndPoint) {
+    public void apiProtocolsActions(String apiEndPoint) throws IOException {
         System.out.println("API Test for end point: " + apiEndPoint);
-        api.setBaseUri();
+        api.setApiEnvironment("ApiBaseUri");
         api.setApiHeader();
         api.createRequestParams();
         api.performPostMethod(apiEndPoint);
         api.getStatusCode();
         api.getResponseBody();
+    }
+
+    @And("^I setup the environment for (.*) API$")
+    public void setupEnvironment(String apiName) throws IOException {
+        api.setApiEnvironment(apiName);
+    }
+
+    @And("^I add the parameter values for (.*) API$")
+    public void addApiParameters(HashMap<String, String> apiParams) {
+        api.createRequestParams();
+    }
+
+    @And("^I perform the (.*) method for the (.*) API$")
+    public void performApiMethod(String method, String endPoint) {
+        api.performPostMethod(endPoint);
+    }
+
+    @Then("^I verify the status code as (.*) for the (.*) API$")
+    public void validateResponseCode(String statusCode, String apiName) {
+
+    }
+
+    @Then("^I verify the response body of (.*) API$")
+    public void validateTheResponseBody(String apiName) {
+
     }
 }
